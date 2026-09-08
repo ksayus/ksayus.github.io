e2eea665ffdd898c9fc82dd5b2bf5fe34b576b40
Ksayus <2481374335@qq.com>
2026-09-09 01:11:20 +0800

    fix

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -91,7 +91,7 @@ export const siteConfig: SiteConfig = {
 	favicon: [
 		{
 			// 图标文件路径
-			src: "/favicon/firefly-32.png",
+			src: "/favicon/ksayus-32.png",
 			// 可选，指定主题 'light' | 'dark'
 			// theme: "light",
 			// 可选，图标大小
